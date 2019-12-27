a6c429e7799abb60a637691645257c6b25466cad
zw <zw@zwdemac.local>
2019-12-27 13:45:02 +0800

    savework

---
+++ src/main/java/com/qiqi/sevice/javasevice/JavaseviceApplication.java
@@ -0,0 +1,13 @@
+package com.qiqi.sevice.javasevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaseviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaseviceApplication.class, args);
+    }
+
+}
